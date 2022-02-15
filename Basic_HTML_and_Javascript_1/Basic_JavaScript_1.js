87ff02161e3fe048702f0603193091c886ef4800
johnrmichals <johnrmichals@gmail.com>
2022-02-15 11:23:42 -0800

    Reorganized Git folders/repositories

---
+++ Basic_HTML_and_Javascript_1/Basic_JavaScript_1.js
@@ -0,0 +1,8 @@
+
+			function My_First_Function()	{
+				var String = "I'm a scary monster!";
+				var result = String.fontcolor("green");
+				document.getElementById("Irish").innerHTML =
+				result;
+				result;
+			}
